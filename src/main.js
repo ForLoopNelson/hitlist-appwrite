3ecd7cc6a5fe64f55921146a231a6bbf7ea9a07d
Cory Nelson <fullnelsontech@gmail.com>
2025-02-14 18:58:42 -0500

    bare bones code.

--- src/main.js
+++ src/main.js
@@ -6,4 +6,12 @@ client.setProject('67afce4b001c520df15a');
 
 const databases = new Databases(client)
 
+const form = document.querySelector('form')
+
+form.addEventListener('submit', addJob)
+
+function addJob(e) {
+  e.preventDefault()
+}
+
 const promise = databases.createDocument()
