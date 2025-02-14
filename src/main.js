1af5826b4b47bc4b716154a02160922f0ac8a1ca
Cory Nelson <fullnelsontech@gmail.com>
2025-02-14 18:47:25 -0500

    first try before creating database

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 import './style.css'
 import { Client } from 'appwrite';
+
+const client = new Client();
+client.setProject('67afce4b001c520df15a');
+
+const databases = new Databases(client)
+
+const promise = databases.createDocument()
